5a34be07b0392c9844c5beec0388e143c49a3d8b
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2017-10-22 19:07:11 +0800

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,34 @@
+// cpanel - site_templates/countdown_fresh/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "1299992@gmail.com",
+        logo: "高考倒计时",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            endTime: '2018-06-06T16:01:00.000Z',
+            content: "",
+        }
+    ]
+};
